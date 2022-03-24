a3f92ba4187c41c618e23e5a667b00f41aa4b93b
Josh Burrows <jburrowsDev@gmail.com>
2022-03-24 16:12:54 -0500

    a

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,20 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 export default function Navigation() {
+  const navigate = useNavigate();
+  const handleContact = (e) => {
+    e.preventDefault();
+    navigate("/contact");
+  };
+  const handleProjects = (e) => {
+    e.preventDefault();
+    navigate("/projects");
+  };
+  const handleAbout = (e) => {
+    e.preventDefault();
+    navigate("/about");
+  };
   return (
     <Navbar bg="dark" fixed="top" variant="dark">
       <Container>
@@ -8,9 +22,9 @@ export default function Navigation() {
           <img alt="logo" src={require("../images/favicon-32x32.png")} />
         </Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="/projects">Projects</Nav.Link>
-          <Nav.Link href="/contact">Contact</Nav.Link>
-          <Nav.Link href="/about">About Me</Nav.Link>
+          <Nav.Link onClick={handleProjects}>Projects</Nav.Link>
+          <Nav.Link onClick={handleContact}>Contact</Nav.Link>
+          <Nav.Link onClick={handleAbout}>About Me</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
